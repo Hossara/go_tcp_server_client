81ac62bdefa542fd7d0436e0ea7938b6b1790ef2
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-10-22 01:53:41 +0330

    feat(App): Improve user scenario

--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"tcp/client"
 	"tcp/server"
 )
 
 func main() {
+	// Initialize a wait group
 	wg := sync.WaitGroup{}
 
+	// Wait for 1 server instance
 	wg.Add(1)
-	go server.NewServer(8080).Connect()
 
+	// Create and connect server instance (listen on localhost port 8080)
+	srv := server.NewServer(8080)
+	go srv.Connect()
+
+	// Create new client instance and connect to server
 	cl := client.NewClient("localhost", 8080)
-	defer cl.Close()
+
+	// Make sure both server connection will be closed at the end
+	defer srv.Close()
+
+	// Connect to client
 	cl.Connect()
 
-	cl.SendMessage("Message1")
-	cl.SendMessage("Message2")
-	cl.SendMessage("Message3")
-	cl.SendMessage("Message4")
-	cl.SendMessage("Message5")
+	for {
+		fmt.Print("Type your message: ")
+		var message string
+
+		fmt.Scanln(&message)
+
+		if message != "end!" {
+			cl.SendMessage(message)
+		} else {
+			cl.Close()
+			wg.Done()
+			break
+		}
+	}
 
 	wg.Wait()
+
+	fmt.Println("Bye Bye!")
 }
